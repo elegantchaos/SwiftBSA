14a99b812e073e8f9d38c80cfa9e6fc8a8b3cde0
Sam Deane <sam@elegantchaos.com>
2022-03-02 13:05:12 +0000

    Added debug output

--- Sources/BSA/hash.py
+++ Sources/BSA/hash.py
@@ -11,25 +11,51 @@ def tesHash(fileName):
     root,ext = os.path.splitext(fileName.lower()) #--"bob.dds" >> root = "bob", ext = ".dds"
     #--Hash1
     chars = map(ord,root) #--'bob' >> chars = [98,111,98]
-    hash1 = chars[-1] | (0,chars[-2])[len(chars)>2]<<8 | len(chars)<<16 | chars[0]<<24
+    print("Arguments")
+    print(root, ext)
+    
+    c1 = chars[-1]
+    c2 = (0,chars[-2])[len(chars)>2]
+    c3 = len(chars)
+    c4 = chars[0]
+    
+    print("Hash1 Components")
+    print(c1, c2, c3, c4)
+    
+    hash1 = c1 | c2<<8 | c3<<16 | c4<<24
     #--(a,b)[test] is similar to test?a:b in C. (Except that evaluation is not shortcut.)
     if   ext == '.kf':  hash1 |= 0x80
     elif ext == '.nif': hash1 |= 0x8000
     elif ext == '.dds': hash1 |= 0x8080
     elif ext == '.wav': hash1 |= 0x80000000
+    
+    print("Hash1")
+    print(hash1)
     #--Hash2
     #--Python integers have no upper limit. Use uintMask to restrict these to 32 bits.
     uintMask, hash2, hash3 = 0xFFFFFFFF, 0, 0
     for char in chars[1:-2]: #--Slice of the chars array
         hash2 = ((hash2 * 0x1003f) + char ) & uintMask
+
+    print("Hash2")
+    print(hash2)
+
     for char in map(ord,ext):
         hash3 = ((hash3 * 0x1003F) + char ) & uintMask
+
+    print("Hash3")
+    print(hash3)
+
     hash2 = (hash2 + hash3) & uintMask
     #--Done
     return (hash2<<32) + hash1 #--Return as uint64
 
 
 if len(sys.argv) == 2:
-    print tesHash(sys.argv[1])
+    hash = tesHash(sys.argv[1])
+    print("")
+    print("Final hash:")
+    print(hash)
+    
 else:
     print "Usage: " + sys.argv[0] + " <path>"
